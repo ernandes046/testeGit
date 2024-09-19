349ebdddffa9fdafe0643db323e80fe8d292c71b
ernandes046 <ernandesoliveira4444@gmail.com>
2024-09-19 19:38:49 -0300

    Initial commit

---
+++ src/br/edu/unisep/testegit/view/Principal.java
@@ -0,0 +1,10 @@
+package br.edu.unisep.testegit.view;
+
+import javax.swing.*;
+
+public class Principal {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null,
+                "mensagem de teste");
+    }
+}
